134acdcdd4ad0725b416cf03a6ccfc114ffa2abe
Ericboo <eric43656@gmail.com>
2021-12-11 14:53:23 -0300

    Correções no código

--- filters.py
+++ filters.py
@@ -1,49 +1,50 @@
 from PIL import Image
 import math as Math
 
-def padding_zero(imagem, size):
-    mask = Math.floor(size / 2)
-    img_alterada = Image.new("RGB", (imagem.width + mask * 2, imagem.height + mask * 2))
-    for x in range(mask, imagem.width + mask):
-        for y in range(mask, imagem.height + mask):
-            img_alterada.putpixel((x, y), value= imagem.getpixel((x - mask, y - mask)))
+def padding_zero(imagem, maskSize):
+    neighbors = Math.floor(maskSize / 2)
+    img_alterada = Image.new("RGB", (imagem.width + neighbors * 2, imagem.height + neighbors * 2))
+    for x in range(neighbors, imagem.width + neighbors):
+        for y in range(neighbors, imagem.height + neighbors):
+            img_alterada.putpixel((x, y), value= imagem.getpixel((x - neighbors, y - neighbors)))
     return img_alterada 
 
-def border_replication(imagem, size):
-    mask = Math.floor(size / 2)
-    img_alterada = Image.new("RGB", (imagem.width + (mask * 2), imagem.height + (mask * 2)))
+
+def border_replication(imagem, maskSize):
+    neighbors = Math.floor(maskSize / 2)
+    img_alterada = Image.new("RGB", (imagem.width + (neighbors * 2), imagem.height + (neighbors * 2)))
     for x in range(img_alterada.width):
         for y in range(img_alterada.height):
-            if (x >= imagem.width + mask or y >= imagem.height + mask):
-                xp = imagem.width if x >= imagem.width + mask else x
-                yp = imagem.height if y >= imagem.height + mask else y
-                img_alterada.putpixel((x, y), value= imagem.getpixel((xp - mask, yp - mask)))
-            elif (x < mask or y < mask):
-                xp = 0 if x <= mask else x - mask
-                yp = 0 if y <= mask else y - mask
+            if (x >= imagem.width + neighbors or y >= imagem.height + neighbors):
+                xp = imagem.width if x >= imagem.width + neighbors else x
+                yp = imagem.height if y >= imagem.height + neighbors else y
+                img_alterada.putpixel((x, y), value= imagem.getpixel((xp - neighbors, yp - neighbors)))
+            elif (x < neighbors or y < neighbors):
+                xp = 0 if x <= neighbors else x - neighbors
+                yp = 0 if y <= neighbors else y - neighbors
                 img_alterada.putpixel((x, y), value= imagem.getpixel((xp, yp)))
             else:
-                img_alterada.putpixel((x, y), value= imagem.getpixel((x - mask, y - mask)))
+                img_alterada.putpixel((x, y), value= imagem.getpixel((x - neighbors, y - neighbors)))
     return img_alterada 
 
 
 
-def average_smoothing(img_alterada, size):
-    mask = Math.floor(size/2)
+def average_smoothing(img_alterada, maskSize):
+    neighbors = Math.floor(maskSize/2)
     #Prepara a criação de uma imagem de saída.
-    for x in range(mask, img_alterada.width - mask):
-        for y in range(mask, img_alterada.height - mask):
+    for x in range(neighbors, img_alterada.width - neighbors):
+        for y in range(neighbors, img_alterada.height - neighbors):
             rsum = 0
             gsum = 0
             bsum = 0
-            for i in range(-mask, mask + 1):
-                for j in range(-mask, mask + 1):
+            for i in range(-neighbors, neighbors + 1):
+                for j in range(-neighbors, neighbors + 1):
                     rsum += img_alterada.getpixel((x + i, y + j))[0]
                     gsum += img_alterada.getpixel((x + i, y + j))[1]
                     bsum += img_alterada.getpixel((x + i, y + j))[2]
-            rmed = int(rsum / size**2)
-            gmed = int(gsum / size**2)
-            bmed = int(bsum / size**2)
+            rmed = int(rsum / maskSize**2)
+            gmed = int(gsum / maskSize**2)
+            bmed = int(bsum / maskSize**2)
             img_alterada.putpixel(
                 (x, y), value=(rmed, gmed, bmed)
             )
@@ -54,14 +55,14 @@ exemplos = []
 img_alteradas = []
 
 #Abre a imagem.
-# exemplos.append(padding_zero(Image.open("exemplo1.jpg"), size=5)) 
-# img_alteradas.append(average_smoothing(exemplos[0], size= 5))
+exemplos.append(padding_zero(Image.open("exemplo1.jpg"), maskSize=3)) 
+img_alteradas.append(average_smoothing(exemplos[0], maskSize= 3))
 
-exemplos.append(border_replication(Image.open("exemplo2.jpg"), size=120)) 
-img_alteradas.append(exemplos[0])
+exemplos.append(border_replication(Image.open("exemplo2.jpg"), maskSize=5)) 
+img_alteradas.append(average_smoothing(exemplos[1], maskSize= 5))
 
 #Monta uma imagem de resultado
 for x in range(0, len(exemplos)):
     resultado = img_alteradas[x]
-    resultado.save("result{}.jpg".format(x))
+    resultado.save("result{}.jpg".format(x + 1))
     resultado.show()
