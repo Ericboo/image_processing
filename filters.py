acf1bcb3b4d96bb0afcae202bd528790841b10c4
Ericboo <eric43656@gmail.com>
2021-12-11 14:00:13 -0300

    border replication parcial adicionado

--- filters.py
+++ filters.py
@@ -2,17 +2,30 @@ from PIL import Image
 import math as Math
 
 def padding_zero(imagem, size):
-    mask = Math.floor(size/2)
-    img_alterada = Image.new("RGB", (imagem.height + mask, imagem.width + mask))
+    mask = Math.floor(size / 2)
+    img_alterada = Image.new("RGB", (imagem.width + mask * 2, imagem.height + mask * 2))
+    for x in range(mask, imagem.width + mask):
+        for y in range(mask, imagem.height + mask):
+            img_alterada.putpixel((x, y), value= imagem.getpixel((x - mask, y - mask)))
+    return img_alterada 
+
+def border_replication(imagem, size):
+    mask = Math.floor(size / 2)
+    img_alterada = Image.new("RGB", (imagem.width + (mask * 2), imagem.height + (mask * 2)))
     for x in range(img_alterada.width):
         for y in range(img_alterada.height):
-            if(x - mask < 0 or y - mask < 0):
-                img_alterada.putpixel((x, y), value= 0)
-            elif (x + mask >= img_alterada.width or y + mask >= img_alterada.height):
-                img_alterada.putpixel((x, y), value= 0)
+            if (x >= imagem.width + mask or y >= imagem.height + mask):
+                xp = imagem.width if x >= imagem.width + mask else x
+                yp = imagem.height if y >= imagem.height + mask else y
+                img_alterada.putpixel((x, y), value= imagem.getpixel((xp - mask, yp - mask)))
+            elif (x < mask or y < mask):
+                xp = 0 if x <= mask else x - mask
+                yp = 0 if y <= mask else y - mask
+                img_alterada.putpixel((x, y), value= imagem.getpixel((xp, yp)))
             else:
                 img_alterada.putpixel((x, y), value= imagem.getpixel((x - mask, y - mask)))
     return img_alterada 
+
 
 
 def average_smoothing(img_alterada, size):
@@ -41,8 +54,11 @@ exemplos = []
 img_alteradas = []
 
 #Abre a imagem.
-exemplos.append(padding_zero(Image.open("exemplo1.jpg"), size=5)) 
-img_alteradas.append(average_smoothing(exemplos[0], size= 5))
+# exemplos.append(padding_zero(Image.open("exemplo1.jpg"), size=5)) 
+# img_alteradas.append(average_smoothing(exemplos[0], size= 5))
+
+exemplos.append(border_replication(Image.open("exemplo2.jpg"), size=120)) 
+img_alteradas.append(exemplos[0])
 
 #Monta uma imagem de resultado
 for x in range(0, len(exemplos)):
